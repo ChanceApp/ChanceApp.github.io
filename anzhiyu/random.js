a17cda44d4dca9d00d33769303ceb692f3842dfc
ChanceApp <532352748@qq.com>
2025-09-13 00:50:30 +0800

    Site updated: 2025-09-13 00:50:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/准备好了么/"];function toRandomPost(){
+var posts=["/Day01-vue3/","/准备好了么/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
